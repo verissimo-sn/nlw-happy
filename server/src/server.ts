6ed3b2b905f0bc11a874c1249b9fb59f315f9008
verissimo-sn <verissimo_sn@hotmail.com>
2020-10-17 14:43:37 -0300

    refactor(mvc): add MVC architecture

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,12 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-
-import Orphanage from './models/Orphanage';
 
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = req.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return res.json({ message: 'Request sent succcesfuly' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('✅ Server running. Port(3333)');
